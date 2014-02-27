42afd14a6ce0d452aec03cb816bcb9f1e7a9f2e9
Alex Tercete <alextercete@gmail.com>
2014-02-27 12:46:02 -0300

    Add global ignore rules for Git

--- create_links.bat
+++ create_links.bat
@@ -17,6 +17,7 @@ call :makeFileSymlink "%home_folder%\.bashrc" "%current_folder%\bashrc"
 call :makeFolderSymlink "%home_folder%\.git_template" "%current_folder%\git_template"
 call :makeFileSymlink "%home_folder%\.gitconfig" "%current_folder%\gitconfig"
 call :makeFileSymlink "%home_folder%\.gitconfig_overrides" "%current_folder%\gitconfig_windows_overrides"
+call :makeFileSymlink "%home_folder%\.gitignore_global" "%current_folder%\gitignore_global"
 
 echo Done!
 
