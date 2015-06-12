41fdaced7dceabda64237fb335ea383627ea17e5
Alex Tercete <alextercete@gmail.com>
2015-06-12 18:32:24 +0100

    Bash: Remember SSH passphrase across terminal sessions

--- bash/.bash.d.symlink/80-ssh-agent.sh
+++ bash/.bash.d.symlink/80-ssh-agent.sh
@@ -1,9 +1,38 @@
 #!/bin/bash
 
-read -p "Do you want to start SSH Agent? [y/N] " answer
-case $answer in
-  Y|y)
-    eval `ssh-agent`>/dev/null && ssh-add
-    ;;
-esac
+environment=~/.ssh/agent_environment
 
+agent_is_running() {
+    if [ "$SSH_AUTH_SOCK" ]; then
+        ssh-add -l >/dev/null 2>&1 || [ $? -eq 1 ]
+    else
+        false
+    fi
+}
+
+agent_has_keys() {
+    ssh-add -l >/dev/null 2>&1
+}
+
+agent_restore() {
+    if [ -f "$environment" ]; then
+        . "$environment" >/dev/null
+    fi
+}
+
+agent_initialize() {
+    (umask 077; ssh-agent >"$environment")
+    . "$environment" >/dev/null
+}
+
+if ! agent_is_running; then
+    agent_restore
+fi
+
+if ! agent_is_running; then
+    agent_initialize && ssh-add
+elif ! agent_has_keys; then
+    ssh-add
+fi
+
+unset environment
