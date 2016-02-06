db96111e388d0a2b3ec494f5eadbacb3e9028f51
Alex Tercete <alextercete@gmail.com>
2016-02-06 19:46:39 +0000

    Bash: Remove old way of loading `__git_ps1`

--- bash/.bash.d.symlink/20-prompt.sh
+++ bash/.bash.d.symlink/20-prompt.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# Make sure `__git_ps1` is available
-if [ -f /usr/share/git/completion/git-prompt.sh ]; then
-    source /usr/share/git/completion/git-prompt.sh
-fi
-
 # <username> <path/to/folder> (<branch>) $
 PS1='\[\e[0;32m\]\u\[\e[m\] \[\e[0;36m\]\w\[\e[m\]\[\e[1;36m\]$(__git_ps1)\[\e[m\] \[\e[1;32m\]\$\[\e[m\] '
