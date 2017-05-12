37acb71c85ae7d15d3d818a2cd50a110d73becde
Alex Tercete <alextercete@gmail.com>
2017-05-12 13:39:49 +0100

    Create `dotfiles` alias

--- .bash.d/10-aliases.sh
+++ .bash.d/10-aliases.sh
@@ -6,6 +6,7 @@ alias gd='git diff'
 alias gds='git diff --staged'
 alias gg='git graph'
 alias npm='npm -s'
+alias dotfiles='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 
 # Custom (usually OS-specific) aliases
 if [ -f ~/.bash_aliases ]; then
