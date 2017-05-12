16ee770170cac0523c6862fb2fbcaf328bccc450
Alex Tercete <alextercete@gmail.com>
2017-05-12 17:02:09 +0100

    Set up autocomplete for Git aliases

--- .bash.d/10-aliases.sh
+++ .bash.d/10-aliases.sh
@@ -17,6 +17,13 @@ alias gg='git graph'
 alias npm='npm -s'
 alias dotfiles='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 
+# Set up autocomplete for Git aliases
+__git_complete gs _git_status
+__git_complete gd _git_diff
+__git_complete gds _git_diff
+__git_complete gg _git_log
+__git_complete dotfiles _git
+
 # OS-specific aliases
 os=$(get_operating_system)
 if [ -f ~/.bash_aliases.$os ]; then
--- .bash_profile
+++ .bash_profile
@@ -4,10 +4,11 @@
 export CLICOLOR=1
 export LSCOLORS=ExGxBxDxCxEgEdxbxgxcxd
 
-# Load ~/.bashrc
-[[ -s ~/.bashrc ]] && source ~/.bashrc
-
 # Load Bash completion
 if hash brew 2>/dev/null; then
     source $(brew --prefix)/etc/bash_completion
 fi
+
+# Load ~/.bashrc
+[[ -s ~/.bashrc ]] && source ~/.bashrc
+
