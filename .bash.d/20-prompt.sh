ab99c8e623fce689c5859b552436d0e765eda53e
Alex Tercete <alextercete@gmail.com>
2018-01-02 21:20:33 +0000

    Bash: Make PS1 declaration more readable

--- .bash.d/20-prompt.sh
+++ .bash.d/20-prompt.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+cyan="\[$(tput setaf 6)\]"
+green="\[$(tput setaf 2)\]"
+bold="\[$(tput bold)\]"
+reset="\[$(tput sgr0)\]"
+
 # <username> <path/to/folder> (<branch>) $
-PS1='\[\e[0;32m\]\u\[\e[m\] \[\e[0;36m\]\w\[\e[m\]\[\e[1;36m\]$(__git_ps1)\[\e[m\] \[\e[1;32m\]\$\[\e[m\] '
+PS1=${green}'\u '${cyan}'\w'
+PS1+=${bold}${cyan}'$(__git_ps1)'
+PS1+=${bold}${green}' \$ '
+PS1+=${reset}
+
+unset cyan green bold reset
