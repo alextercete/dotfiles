eeff2866093c6dce9d99a26adb3c84541364c652
Alex Tercete <alextercete@gmail.com>
2018-01-02 21:22:49 +0000

    Bash: Add new-line character before prompt
    The new-line character needs to be escaped using the `$''` syntax,
    otherwise an error occurs in Git Bash.
    
    See: https://stackoverflow.com/a/21561763/1288760

--- .bash.d/20-prompt.sh
+++ .bash.d/20-prompt.sh
@@ -5,10 +5,11 @@ green="\[$(tput setaf 2)\]"
 bold="\[$(tput bold)\]"
 reset="\[$(tput sgr0)\]"
 
-# <username> <path/to/folder> (<branch>) $
+# <username> <path/to/folder> (<branch>)
+# $
 PS1=${green}'\u '${cyan}'\w'
 PS1+=${bold}${cyan}'$(__git_ps1)'
-PS1+=${bold}${green}' \$ '
+PS1+=${bold}${green}$'\n$ '
 PS1+=${reset}
 
 unset cyan green bold reset
