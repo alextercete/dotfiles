d0979c75866b6384f5cf702d6343d1748d9a80ff
Alex Tercete <alextercete@gmail.com>
2019-08-12 13:14:22 +0100

    Bash: Ensure Git completions work on Ubuntu

--- .bash.d/09-completion.sh
+++ .bash.d/09-completion.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Load Bash completion for Ubuntu if present
+if [ -f /usr/share/bash-completion/completions/git ]; then
+  source /usr/share/bash-completion/completions/git
+fi
+
 # Load Bash completion from Homebrew if present
 if hash brew 2>/dev/null; then
   source "$(brew --prefix)/etc/bash_completion"
